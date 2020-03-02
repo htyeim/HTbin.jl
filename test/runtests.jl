702e34ff7cb2eeebd5de7b4a631d315b770b5f24
htyeim <htyeim@gmail.com>
2020-03-02 14:59:56 +0800

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using HTbin
+using Dates
 using Test
 
-@testset "HTbin.jl" begin
-    # Write your own tests here.
+@time @testset "HTbin.jl Package Tests" begin
+
+    testdir = joinpath(dirname(@__DIR__), "test")
+    @time @testset "so_files" begin
+        include(joinpath(testdir, "so_files.jl"))
+    end
 end

998c1af476a010addb4a3ef9d8a0ab16c330b294
htyeim <htyeim@gmail.com>
2021-01-15 14:31:27 +0800

    1

--- src/bin/RNXCMP/_wrapper.jl
+++ src/bin/RNXCMP/_wrapper.jl
@@ -2,7 +2,7 @@
 
 const RNXCMPVER = isapple ? "RNXCMP_4.0.8_MacOSX10.14_gcc" :
                         islinux ? "RNXCMP_4.0.8_Linux_x86_64bit" :
-                            throw("no windows!")
+                                throw("no windows!")
 
 const CRX2RNX    = string(@__DIR__, "/", RNXCMPVER, "/bin/CRX2RNX")
 const RNX2CRX    = islinux ? 
